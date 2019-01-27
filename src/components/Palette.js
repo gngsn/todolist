172f738cf05dbadc6ba0ae248d63fde52aa17883
tomas4540 <rudtjs4540@gmail.com>
2019-01-27 23:33:08 +0900

    modify CSS

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -3,14 +3,14 @@ import './Palette.css';
 
 const Color = ({ color, active, onClick }) => {
     return (
-        <div className="color" style ={{ background: color}} onClick={onClick}>
+        <div className={`color ${active && 'active'}`} style={{ background: color}} onClick={onClick}>
         </div>
     );
 };
 
 const Palette = ({colors, selected, onSelect}) => {
     const colorList = colors.map(
-        (color) => (<Color color ={color} onClick={() => onSelect(color)} key ={color} />));
+        (color) => (<Color color ={color} active={selected===color} onClick={() => onSelect(color)} key ={color} />));
     return(
         <div className="palette">
             {colorList}
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -11,7 +11,7 @@ class TodoItemList extends Component {
         const { todos, onToggle, onRemove } = this.props;
         const todoList = todos.map(
             ({ id, text, checked, color }) => (
-                <div className="todo-item" key={id} onClick={() => onToggle(id)}>
+                <div className="todo-item" key={id} >
                     <div className="remove" onClick={(e) => {
                         e.stopPropagation(); // onToggle 이 실행되지 않도록 함
                         onRemove(id)}
@@ -19,9 +19,14 @@ class TodoItemList extends Component {
                     <div className={`todo-text ${checked && 'checked'}`} style={{color: color}}>
                         <div>{text}</div>
                     </div>
-                    {
-                        checked && (<div className="check-mark">✓</div>)
-                    }
+                    <div onClick={() => onToggle(id)}>
+                        {
+                            !checked && (<div className="check-square">□</div>)
+                        }
+                        {
+                            checked && (<div className="check-mark">✓</div>)
+                        }
+                    </div>
                 </div>
             )
         );
